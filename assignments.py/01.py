5bf70e22f6ff70df7ee8b3eff2c7ab508161137b
codewithjaiganesh <jaiganeshreddy69@gmail.com>
2026-06-06 16:11:29 +0530

    06

---
+++ assignments.py/01.py
@@ -0,0 +1,6 @@
+#TO CHECK THE GIVEN STRING IS PALINDROME
+input_user = input("enter the string to check palindrome: ")
+if(input_user[::-1] == input_user):
+    print(f"The word {input_user} is a palindrome")
+else:
+    print("its not a palindrome!")
