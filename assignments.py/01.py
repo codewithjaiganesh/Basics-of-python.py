737eaf2487412a7c8c06ad70b1e4289560b3ba10
codewithjaiganesh <jaiganeshreddy69@gmail.com>
2026-06-07 10:39:57 +0530

    06

--- assignments.py/01.py
+++ assignments.py/01.py
@@ -1,5 +1,5 @@
 #TO CHECK THE GIVEN STRING IS PALINDROME
-input_user = input("enter the string to check palindrome: ")
+input_user = input("enter the string to check palindrome: ").lower()
 if(input_user[::-1] == input_user):
     print(f"The word {input_user} is a palindrome")
 else:
