6092e93c37fac64263b6c133e81facce9d924174
codewithjaiganesh <jaiganeshreddy69@gmail.com>
2026-07-11 12:04:42 +0530

    clicke here

--- assignments.py/asigments05/printUsersName.py
+++ assignments.py/asigments05/printUsersName.py
@@ -1,6 +1,6 @@
 data = True
 with open(r"D:\XboxGames\githubb\Basics-of-python.py\assignments.py\asigments05\names.txt","w") as f:
-    f.writelines("Jai\nGanesh\n")
+    f.writelines("Jai\nGanesh\nbhanu\nraja\ngow\n")
 with open(r"D:\XboxGames\githubb\Basics-of-python.py\assignments.py\asigments05\names.txt","r+") as p:
     content = p.readlines()
     print(content)
